e08687963aef79730df5f090fd1d50ee258cc2e2
geunhokinn <gghh1220@gmail.com>
2024-05-27 22:29:57 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/jpql/JpaMain.java
@@ -0,0 +1,16 @@
+package jpql;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        //code
+
+        em.close();
+        emf.close();
+    }
+}
