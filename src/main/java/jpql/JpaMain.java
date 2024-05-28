29b9f84c9d6cb3d132cff523e84b68c0dc0e3e14
geunhokinn <gghh1220@gmail.com>
2024-05-28 21:36:55 +0900

    feat: 벌크 연산

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -28,29 +28,31 @@ public class JpaMain {
 
             Member member1 = new Member();
             member1.setUsername("회원1");
+            member1.setAge(0);
             member1.setTeam(teamA);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("회원2");
+            member2.setAge(0);
             member2.setTeam(teamA);
             em.persist(member2);
 
             Member member3 = new Member();
             member3.setUsername("회원3");
+            member3.setAge(0);
             member3.setTeam(teamB);
             em.persist(member3);
 
-            em.flush();
-            em.clear();
+            // 자동 flush
+            int resultCount = em.createQuery("update Member m set m.age = 20")
+                    .executeUpdate();
+            // DB 반영
 
-            List<Member> members = em.createNamedQuery("Member.findByUsername", Member.class)
-                    .setParameter("username", "회원1")
-                    .getResultList();
+            em.clear(); // 초기화, 영속성 컨텍스트에서 다시 가져옴
 
-            for (Member member : members) {
-                System.out.println("member = " + member);
-            }
+            Member findMember = em.find(Member.class, member1.getId());
+            System.out.println("findMember.getAge() = " + findMember.getAge());
 
             tx.commit();
         } catch (Exception e) {
