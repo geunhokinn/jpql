b995615b081a2d8f8f22703a9a640813bf69434c
geunhokinn <gghh1220@gmail.com>
2024-05-28 11:25:59 +0900

    feat: 조인

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -16,25 +16,27 @@ public class JpaMain {
 
         try {
 
-            for (int i=0; i<100; i++) {
-                Member member = new Member();
-                member.setUsername("member" + i);
-                member.setAge(i);
-                em.persist(member);
-            }
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("teamA");
+            member.setAge(10);
+
+            member.changeTeam(team);
+
+            em.persist(member);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(1) // offset
-                    .setMaxResults(10) // limit
-                    .getResultList();
-
-            System.out.println("result.size() = " + result.size());
-            for (Member member : result) {
-                System.out.println("member = " + member);
-            }
+            // 조인 대상 필터링
+//            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
+            // 연관관계 엾는 엔티티 외부 조인
+            String query = "select m from Member m left join Team t on m.username = t.name";
+            List<Member> result = em.createQuery(query, Member.class)
+                            .getResultList();
 
             tx.commit();
         } catch (Exception e) {
